2c0308ce0e584ac10922ae635eecfbf698e6c49a
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-11-28 21:46:27 +0600

    footer corrected

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -34,7 +34,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/category/:id',
-                loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`),
+                loader: ({ params }) => fetch(`https://bikers-station-server.vercel.app/category/${params.id}`),
                 element: <PrivateRoute><Products></Products></PrivateRoute>
             }
         ]
